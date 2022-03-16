8a0558b9d9fa56a33fc55479097ae52b9acbd210
Gabriel Hernandez <pagabo18@hotmail.com>
2022-03-16 07:37:34 -0600

    LinkA edited

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,14 @@ const userRoutes = require("./../modules/users/user.routes");
 const messageRoutes = require("../modules/messages/messages.routes");
 const rolesRoutes = require("./../modules/roles/roles.routes");
 const roomsRoutes = require('./../modules/rooms/rooms.routes');
+const linkARoutes = require('./../modules/linkA/linkA.routes');
 // const groupRoutes = require("../modules/groups/groups.routes");
 
 router.use("/users", userRoutes);
 router.use("/messages", messageRoutes);
 router.use("/roles", rolesRoutes);
 router.use('/rooms', roomsRoutes);
+router.use('/linkAs', linkARoutes);
 // router.use("/group", groupRoutes);
 
 
